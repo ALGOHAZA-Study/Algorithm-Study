929e056e06543b77464839529c58833c55832df6
jbw9964 <jbw9964@gmail.com>
2024-04-16 03:20:07 +0900

    `문제 정리 구조`, `작성` 예시 작성 완료

---
+++ jbw9964/M_2024_04/Week_2/P_2609/Solution_2609.java
@@ -0,0 +1,37 @@
+package jbw9964.M_2024_04.Week_2.P_2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_2609 {
+
+    private static final BufferedReader br = new BufferedReader(
+        new InputStreamReader(System.in)
+    );
+
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        
+        int num1 = Integer.parseInt(tokenizer.nextToken());
+        int num2 = Integer.parseInt(tokenizer.nextToken());
+
+        int gcd = GCD(num1, num2);
+        int lcm = num1 * num2 / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    public static int GCD(int a, int b) {       // get Greatest common divisor using euclidean algorithm
+        while (b != 0) {
+            int mod = a % b;
+            a = b;
+            b = mod;
+        }
+        
+        return a;
+    }
+}
